e040fdeda4c0de8756c9bcb8ab344a8f81a6b045
Nofy Leclercq <nolecler@3A4.42angouleme.fr>
2025-01-17 16:40:14 +0100

    collect ONE carrot OK

--- controls.c
+++ controls.c
@@ -6,7 +6,7 @@
 /*   By: nolecler <nolecler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 15:36:01 by nolecler          #+#    #+#             */
-/*   Updated: 2025/01/17 15:16:20 by nolecler         ###   ########.fr       */
+/*   Updated: 2025/01/17 16:36:12 by nolecler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,29 @@
 
 
 // //game->images.image_rabit->instances[0].x = game->player_y * SPRITE_PIXEL;
-// void	collect_carot(t_game *game)
-// {
-
-// 	// verifier que la carotte et le P = meme position;
-// 	// si oui : l'image de carotte disparait (enabled = false);
-// 	// marquer la case comme vide = '0';
-// 	// count_c--;
-// 	game->images.image_carot->instances[1].x = game->collectible_y * SPRITE_PIXEL;
-// 	game->images.image_carot->instances[1].y = game->collectible_x * SPRITE_PIXEL;
-
-// 	// if ((game->images.image_carot->instances[1].x == game->images.image_rabit->instances[0].x) &&
-// 	// 	(game->images.image_carot->instances[1].y ==  game->images.image_rabit->instances[0].y))
+void	collect_carot(t_game *game)
+{
+	// verifier que la carotte et le P = meme position;
+	// si oui : l'image de carotte disparait (enabled = false);
+	// marquer la case comme vide = '0';
+	// count_c--;
 	
-// 	if (game->map[game->player_x][game->player_y] == 'C')
-// 	{
-// 		game->images.image_carot->instances[1].enabled = false;
-// 		// mlx_image_to_window(game->mlx, game->images.image_floor,
-// 		// 	game->player_x, game->player_y);
-// 		// mlx_image_to_window(game->mlx, game->images.image_rabit_end,
-// 		// 	game->player_x, game->player_y);
-// 		game->count_c--;	
-// 	}
-// 	// if (game->count_c == 0)
-// 	// {
-// 	// 	mlx_image_to_window(game->mlx, game->images.image_rabit_end,
-// 	// 		game->gate_x, game->gate_y);
-// 	// 	if (game->map[(game->player_x)][(game->player_y)] == 'E')
-// 	// 		mlx_close_window(game->mlx);
-// 	// }
-// }
+		game->images.image_carot->instances[0].enabled = false;
+		// regarder les coordoonees de chaque image
+		game->map[game->player_y][game->player_x] = '0';
+		game->count_c--;
+	
+
+}
+
+	// if (game->count_c == 0)
+	// {
+	// 	mlx_image_to_window(game->mlx, game->images.image_rabit_end,
+	// 		game->gate_x, game->gate_y);
+	// 	if (game->map[(game->player_x)][(game->player_y)] == 'E')
+	// 		mlx_close_window(game->mlx);
+	// }
+
 
 
 void move(t_game *game, int key)
@@ -68,8 +61,9 @@ void move(t_game *game, int key)
         if (game->map[game->player_y + 1][game->player_x] != '1')
             move_down(game);
     }
-	// if (game->map[game->player_x][game->player_y] == 'C')
-	// 	collect_carot(game);
+	if (game->map[game->player_y][game->player_x] == 'C')
+		collect_carot(game);
+	
 }
 
 void	key_events(mlx_key_data_t keydata, void *param)
